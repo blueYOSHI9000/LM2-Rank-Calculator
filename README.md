86b0053b3547af61048360ab9fcede3ce274b354
blueYOSHI <35354437+blueYOSHI9000@users.noreply.github.com>
2025-04-20 19:01:52 +0200

    Initial version
    Still wanna add a list of levels, make the inputs look better, add credits on the page and other smaller stuff but this *is* usable already
    
    Added: full functionality

---
+++ README.md
@@ -0,0 +1,13 @@
+
+## Todo:
+- add list of levels for a quick select
+- add credits & calculation explanation on site
+- better looking inputs
+- a better README, I suppose
+
+## Credits
+- Score calculations & list of score requirements: https://www.reddit.com/r/LuigisMansion/comments/1cp0ah6/comment/l3om03i/
+- Background image: Mariowiki
+- Sprites: https://www.spriters-resource.com/3ds/luigismansion2/sheet/122133/ & https://www.spriters-resource.com/3ds/luigismansion2/sheet/122134/
+- Adlib font: https://www.fontspace.com/adlib-font-f157 (originally made by Freeman Craw for ATF)
+- LM-style rectangles: https://stackoverflow.com/questions/59813538/css-custom-shape-with-irregular-rectangle-and-border
---
+++ script.js
@@ -0,0 +1,42 @@
+
+function rankCalc () {
+	const timeH = Number(document.getElementById('timeH').value);
+	const timeMin = Number(document.getElementById('timeMin').value);
+	const timeS = Number(document.getElementById('timeS').value);
+
+	const ghostsCaptured = Number(document.getElementById('ghostsCaptured').value);
+	const healthLost = Number(document.getElementById('healthLost').value);
+	const collectedTreasure = Number(document.getElementById('collectedTreasure').value);
+
+	const score = ghostsCaptured*120 + collectedTreasure - ((timeH*60 + timeMin)*60 + timeS) - healthLost*6;
+
+	const silverGoal = Number(document.getElementById('silverGoal').value);
+	const goldGoal = Number(document.getElementById('goldGoal').value);
+	const goalDiff = goldGoal - silverGoal;
+
+	//in decimal, so 0.5 instead of 50%
+	const progress = (score + goalDiff) / ((goldGoal - silverGoal)*2);
+
+	const minMaxProgress = (progress < 0) ? 0 : (progress > 1) ? 1 : progress
+
+	//if gold is gotten, fill it all up - otherwise use a 10px gap on the right so it's clearly visible gold was *NOT* gotten
+		//also uses a 10px gap on the left even when it's 0% as there's no true 0% in this calculation
+
+	//this makes it so the bar snaps on both ends - the progress is all between bronze & gold, below bronze it snaps to 0, above gold it snaps to 100%
+		//gold - 1 point leaves a 10px gap on the right that gets filled completely with just 2 extra points
+	document.getElementById('progressBar'      ).style.width = (progress < 0) ? '10px' : ((progress > 1) ? '100%' : `calc((100% - 20px) * ${minMaxProgress} + 10px)`);
+	document.getElementById('finalScoreDisplay').style.left  = (progress < 0) ? 'calc(10px - 50px)' : ((progress > 1) ? 'calc(100% - 50px)' : `calc((100% - 20px) * ${minMaxProgress} + 10px - 50px)`);
+
+	document.getElementById('finalScoreDisplay').style.visibility = 'visible';
+	document.getElementById('finalScore').value = score;
+
+	if (score >= goldGoal) {
+		document.getElementById('progressBar').classList.add('goldReached');
+	} else if (score >= silverGoal) {
+		document.getElementById('progressBar').classList.add('silverReached');
+		document.getElementById('progressBar').classList.remove('goldReached');
+	} else {
+		document.getElementById('progressBar').classList.remove('silverReached');
+		document.getElementById('progressBar').classList.remove('goldReached');
+	}
+}
