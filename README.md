c5f31b509c35e53bef917f75345f11a5cef1a070
blueYOSHI <35354437+blueYOSHI9000@users.noreply.github.com>
2025-04-26 00:30:07 +0200

    Better Number Inputs!!
    Changed: Improved the look of the number inputs, mainly the up & down arrows!
    Changed: Non-numbers are no longer accepted input.
    Changed: Minute & Seconds now pad the number with a leading 0 and no longer allow number over 60.

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-# Luigi's Mansion: Dark Moon - Rank Calculator
+# Luigi's Mansion 2: Dark Moon - Rank Calculator
 Does what it says, calculates your rank for a mission but more specifically, it shows the exact score so you *know* how close or far away you were.  
 No, I don't expect this to really be useful, I just felt like making it so here you go. Enjoy. Or else.
 
 ## Todo:
 - add list of levels for a quick select
 - add credits & calculation explanation on site
-- better looking inputs (no ugly ^ v buttons & time padded to 01 instead of 1)
+- better looking disabled inputs (score)
 - automatic calculations, no button-press needed
 - better Progressbar look
 
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+/* Calculates the rank & updates the display */
 function rankCalc () {
 	const timeH = Number(document.getElementById('timeH').value);
 	const timeMin = Number(document.getElementById('timeMin').value);
@@ -48,3 +48,274 @@ function rankCalc () {
 		document.getElementById('finalScoreArrow').style.backgroundColor = 'var(--bronze)';
 	}
 }
+
+/**	Adds 1 to an <input> element.
+ *
+ * 	Args:
+ * 		elem [HTMLElement]
+ * 			The <input> element.
+ */
+function valuePlus (elem) {
+	if (!(elem instanceof HTMLElement)) {
+		console.warn(`No valid element found.`);
+		return;
+	}
+
+	elem = elem.getRootNode().getElementById('liInput');
+	if (elem === null) {
+		console.warn(`Could not find input element.`);
+		return;
+	}
+
+	if (elem.getAttribute('disabled') !== null)
+		return;
+
+	elem.value = Number(elem.value) + 1;
+
+	validateInput(elem);
+}
+
+/**	Removes 1 from an <input> element.
+ *
+ * 	Args:
+ * 		elem [HTMLElement]
+ * 			The <input> element.
+ */
+function valueMinus (elem) {
+	if (!elem instanceof HTMLElement) {
+		console.warn(`No valid element found.`);
+		return;
+	}
+
+	elem = elem.getRootNode().getElementById('liInput');
+	if (elem === null) {
+		console.warn(`Could not find input element.`);
+		return;
+	}
+
+	if (elem.getAttribute('disabled') !== null)
+		return;
+
+	elem.value = Number(elem.value) - 1;
+
+	validateInput(elem);
+}
+
+/** Validates the input.
+ *
+ * 	Removes non-numbers.
+ * 	Keeps cursor intact.
+ * 	Checks to "min" & "max" & "pad" attributes (ignored with 'oninput').
+ *
+ * 	Args:
+ * 		elem [HTMLElement]
+ * 			The <input> element to be validated.
+ *
+ * 		oninput [Boolean] <true>
+ * 			If false it handles extra stuff (mainly padding) that isn't done with every input.
+ */
+function validateInput (elem, oninput) {
+	if (!elem instanceof HTMLElement) {
+		console.warn(`Could not find input element.`);
+		return;
+	}
+	const min = elem.getAttribute('min') === null ? null : Number(elem.getAttribute('min'));
+	const max = elem.getAttribute('max') === null ? null : Number(elem.getAttribute('max'));
+	const pad = elem.getAttribute('pad') === null ? 1    : Number(elem.getAttribute('pad')); //for leading zeroes, indicates expected length; 5 = number needs to be 5 digits long, pad with 0
+	let cursorPosition = Number(elem.selectionStart);
+
+	let value = elem.value;
+	if (typeof value !== 'string') {
+		console.warn('Not String found.');
+		return;
+	}
+
+	if (oninput !== true) {
+		value = Number(value);
+
+		if (typeof min === 'number' && Number(value) < min) {
+			value = min;
+		}
+		if (typeof max === 'number' && Number(value) > max) {
+			value = max;
+		}
+	}
+
+	value = value.toString().split('');
+
+	//remove non-numbers but allow a single leading -
+	for (let index = value.length - 1; index >= 0; index--) {
+		if (isNaN(value[index]) && !(value[index] === '-' && index === 0)) {
+			value.splice(index, 1);
+			cursorPosition--; //counteract the removal
+		}
+	}
+
+	if (oninput !== true) {
+
+		//count leading zeroes
+		let digits = 0;
+		let leadingZeroes = 0;
+		let nonZeroSpotted = false;
+		for (let i of value) {
+			if (+i === 0 && nonZeroSpotted === false) {
+				leadingZeroes++;
+			} else if (i !== '-') {
+				nonZeroSpotted = true;
+			}
+
+			if (i !== '-') {
+				digits++;
+			}
+		}
+
+		//add leading zeroes
+		if (digits < pad) {
+			const padDiff = pad - digits;
+			let padArr = [];
+			for (let i = 0; i < padDiff; i++) {
+				padArr.push(0);
+			}
+
+			if (value[0] === '-') {
+				value.splice(1, 0, ...padArr)
+			} else {
+				value.splice(0, 0, ...padArr)
+			}
+
+			//keep the cursor position intact despite the additions
+			cursorPosition = cursorPosition + padDiff;
+
+		//remove leading zeroes above the pad
+		} else if (digits > pad && leadingZeroes > 0) {
+			let toSplice = digits - pad;
+			if (toSplice < 0 || toSplice > leadingZeroes) {
+				toSplice = leadingZeroes;
+			}
+
+			if (value[0] === '-') {
+				value.splice(1, toSplice);
+			} else {
+				value.splice(0, toSplice);
+			}
+
+			//keep the cursor position intact despite the removed entries
+			cursorPosition = cursorPosition - toSplice;
+		}
+	}
+
+	value = value.join('');
+
+	if (isNaN(value) && !(oninput === true && value === '-')) {
+		debugger;console.warn(`Got a NaN here.`);
+		value = 0;
+	}
+
+	elem.value = value;
+	elem.selectionStart = cursorPosition;
+	elem.selectionEnd = cursorPosition;
+}
+
+/**	<luigi-input>
+ *
+ * 	Use "mix" & "max" attributes to specifiy a number range.
+ * 	Use "pad" attribute to pad the number with leading zeroes to that length. A "pad" value of 5 will the number "10" into "00010". Defaults to 1.
+ */
+class LuigiInput extends HTMLElement {
+	constructor() {
+		super();
+		//console.log('constructo');
+
+		const shadow = this.attachShadow({ mode: "open" });
+
+		const cssLink = document.createElement('link');
+		cssLink.rel = "stylesheet";
+		cssLink.href = "./style.css";
+		shadow.appendChild(cssLink);
+
+		const container = document.createElement('div');
+		shadow.appendChild(container);
+
+
+		const textInput = document.createElement('input');
+		textInput.id = 'liInput';
+		//textInput.value = this.getAttribute('value'); //Isn't loaded yet because everything is stupid I guess.....
+		textInput.setAttribute('oninput', 'validateInput(this, true)');
+		textInput.setAttribute('onchange', 'validateInput(this)');
+		container.appendChild(textInput);
+
+		const arrows = document.createElement('span');
+		arrows.classList.add('liArrows');
+		container.appendChild(arrows);
+
+		const   upArrowContainer = document.createElement('span');
+		const downArrowContainer = document.createElement('span');
+		  upArrowContainer.classList.add(  'liUpArrowContainer');
+		downArrowContainer.classList.add('liDownArrowContainer');
+		  upArrowContainer.setAttribute('onclick', 'valuePlus(this);');
+		downArrowContainer.setAttribute('onclick', 'valueMinus(this);');
+		arrows.appendChild(  upArrowContainer);
+		arrows.appendChild(downArrowContainer);
+
+		const   upArrow = document.createElement('span');
+		const downArrow = document.createElement('span');
+		  upArrow.classList.add(  'liUpArrow');
+		downArrow.classList.add('liDownArrow');
+		  upArrowContainer.appendChild(  upArrow);
+		downArrowContainer.appendChild(downArrow);
+	}
+
+	get value () {
+		const elem = this.shadowRoot.getElementById('liInput');
+		if (!elem instanceof HTMLElement) {
+			console.error(`No element found.`);
+			return;
+		}
+		return elem.value;
+	}
+
+	set value (val) {
+		const elem = this.shadowRoot.getElementById('liInput');
+		if (!elem instanceof HTMLElement) {
+			console.error(`No element found.`);
+			return;
+		}
+		elem.value = val;
+		validateInput(elem);
+	}
+
+	connectedCallback() {
+		//console.log('connecto');
+
+		const newValue = this.getAttribute('value');
+		if (!isNaN(newValue)) {
+			this.shadowRoot.getElementById('liInput').value = newValue;
+		}
+
+		validateInput(this.shadowRoot.getElementById('liInput'));
+
+		return;
+	}
+
+	disconnectedCallback() {
+		console.log('disconnecto');return;
+	}
+
+	connectedMoveCallback() {
+		console.log('Icky paint like goop, it"s *moooovin*!');return;
+	}
+
+	static observedAttributes = ['min', 'max', 'pad', 'disabled'];
+
+	attributeChangedCallback(name, oldValue, newValue) {
+		const elem = this.shadowRoot.getElementById('liInput');
+		if (!elem instanceof HTMLElement) {
+			console.error(`No element found.`);
+			return;
+		}
+		elem.setAttribute(name, newValue);
+		validateInput(elem);
+	}
+}
+customElements.define("luigi-input", LuigiInput);
+
