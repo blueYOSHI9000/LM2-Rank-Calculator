d38baa708b0f61c2ebb65de148742ecabcbaef1b
blueYOSHI <35354437+blueYOSHI9000@users.noreply.github.com>
2025-05-17 13:21:39 +0200

    Fix the cache.
    I think.
    I hope.

--- sw.js
+++ sw.js
@@ -8,6 +8,8 @@ self.addEventListener("install", (e) => {
 	console.log("[Service Worker] Install");
 	e.waitUntil(
 		(async () => {
+			await caches.delete("yoshmincom-LM2RC-v1");
+
 			const cache = await caches.open(cacheName);
 			console.log("[Service Worker] Caching all: app shell and content");
 			//abismal solution but given Chrome's surprising abundance of stupidity in reporting *which* fucking file failed and the fact it works perfectly fine in a localhost, this is more or less the only way to see which file failed to load.
